eb4c1b8182c6bf007ee11fbc60beccd8d7e05293
AliSharafi88 <alisharafi893@gmail.com>
2024-09-21 12:47:42 +0330

    change str method of order to customer user.

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from courses.models import Course
 
 
@@ -36,7 +36,7 @@ class Order(models.Model):
     total_price = models.DecimalField(_('total price'), max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
-        return f'{self.id} - {self.customer}'
+        return self.customer.username
 
 
 class DVDOrderDetail(models.Model):
