8cae63eca514f567efae69eee92eed23ade7063e
AliSharafi88 <alisharafi893@gmail.com>
2024-10-03 15:25:41 +0330

    change orders.js.

--- static/assets/js/orders.js
+++ static/assets/js/orders.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    console.log($('input[name="status"]').attr('name'));
     $('input[name="status"]').change(function () {
-        console.log(this.value);
         if (this.value === 'd') {
             $('#buy-dvd-section').show();
+            $('#buy-dvd-section input, #buy-dvd-section textarea').attr('required', true);
         } else {
             $('#buy-dvd-section').hide();
+            $('#buy-dvd-section input, #buy-dvd-section textarea').attr('required', false);
         }
-    });
+    }).change();
 });
