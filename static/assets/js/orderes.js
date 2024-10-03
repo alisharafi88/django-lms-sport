29f096ff1e1a0a87fde73b12e64c57d88f9447fc
AliSharafi88 <alisharafi893@gmail.com>
2024-10-03 15:28:39 +0330

    change orders.js again for new bug.

--- static/assets/js/orderes.js
+++ static/assets/js/orderes.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
-    $('input[name="status"]').change(function () {
-        if (this.value === 'd') {
+    function toggleDvdSection() {
+        const isDvdSelected = $('input[name="status"]:checked').val() === 'd';
+        if (isDvdSelected) {
             $('#buy-dvd-section').show();
             $('#buy-dvd-section input, #buy-dvd-section textarea').attr('required', true);
         } else {
             $('#buy-dvd-section').hide();
             $('#buy-dvd-section input, #buy-dvd-section textarea').attr('required', false);
         }
-    }).change();
+    }
+
+    $('input[name="status"]').change(toggleDvdSection);
+
+    toggleDvdSection();
 });
