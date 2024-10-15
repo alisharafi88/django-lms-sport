3cddbb9e4d5c22b6b37cb563dff2b21bdcc7b27d
AliSharafi88 <alisharafi893@gmail.com>
2024-10-15 19:36:31 +0330

    change home page queries to have better performance and add queries to have better view

--- home/views.py
+++ home/views.py
@@ -1,26 +1,23 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
 
-from courses.models import Course
-from instructors.models import Instructor
-from blogs.models import Blog
+from .queries import get_top_courses, get_featured_instructors, get_latest_blogs
 
 
 class HomeView(View):
     template_name = 'home/home.html'
 
     def get(self, request):
-        courses_queryset = Course.objects.filter(status=True)[:3]
-        instructors_queryset = Instructor.objects.select_related('user').filter(status=True)[:3]
-        blogs_queryset = Blog.objects.select_related('author__user').filter(status=Blog.BLOG_STATUS_PUBLISHED)[:3]
+        top_courses_query = get_top_courses()
+        featured_instructors_query = get_featured_instructors()
+        latest_blogs_query = get_latest_blogs()
         return render(
             request,
             self.template_name,
             {
-                'courses': courses_queryset,
-                'instructors': instructors_queryset,
-                'blogs': blogs_queryset,
+                'courses': top_courses_query,
+                'instructors': featured_instructors_query,
+                'blogs': latest_blogs_query,
             }
         )
 
@@ -29,5 +26,5 @@ class AboutUsView(View):
     template_name = 'home/aboutus.html'
 
     def get(self, request):
-        instructors_queryset = Instructor.objects.select_related('user').filter(status=True)[:3]
-        return render(request, self.template_name, {'instructors': instructors_queryset,})
+        featured_instructors_query = get_featured_instructors()
+        return render(request, self.template_name, {'instructors': featured_instructors_query})
