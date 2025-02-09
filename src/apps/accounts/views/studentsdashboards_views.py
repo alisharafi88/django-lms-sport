bfc2f78ce1a7dc841d7c5753c5e66787505b739b
AliSharafi88 <alisharafi893@gmail.com>
2025-02-09 22:31:36 +0330

    Update student dashboard and related static files and implemet liked part

--- src/apps/accounts/views/studentsdashboards_views.py
+++ src/apps/accounts/views/studentsdashboards_views.py
@@ -1,10 +1,23 @@
+from django.db.models import Subquery
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from apps.courses.models import Course, CourseLike
 
 
-
-class StudentsDashboard(View):
+class StudentsDashboard(LoginRequiredMixin, View):
     template_name = 'accounts/dashboards/student_dashboard.html'
     
     def get(self, request):
-        return render(request, self.template_name)
+        liked_course_queryset = Course.objects.filter(id__in=Subquery(CourseLike.objects.filter(user_id=request.user.id).values('course')))
+
+        context = {
+            'liked_courses': liked_course_queryset,
+        }
+
+        return render(
+            request,
+            self.template_name,
+            context,
+        )
