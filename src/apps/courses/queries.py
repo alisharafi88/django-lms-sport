365fb5d2af8cd4002fcd2c11cf7b5da0cfc35572
AliSharafi88 <alisharafi893@gmail.com>
2025-02-22 12:54:43 +0330

    Update courses model and add query file for courses and packages combined queryset.

---
+++ src/apps/courses/queries.py
@@ -0,0 +1,65 @@
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Case, When, Value, F, CharField, Q, Count, IntegerField, Avg
+from django.db.models.functions import Concat
+
+from .models import Course, Package
+
+
+def get_courses_queryset():
+    """
+        Retrieve a queryset of active courses with annotated fields.
+    """
+    course_content_type = ContentType.objects.get_for_model(Course)
+    queryset = Course.objects.filter(status=True).annotate(
+        coach_profile=Case(
+            When(coach__user__profile_photo__isnull=False,
+                 then=Concat(Value(settings.MEDIA_URL), F("coach__user__profile_photo"), output_field=CharField())),
+            output_field=CharField(),
+        ),
+        num_members=Count('memberships', filter=Q(memberships__content_type=course_content_type), distinct=True),
+        num_videos=Count('seasons__videos'),
+        product_type=Value(1, output_field=IntegerField()),  # 1 = course
+        num_courses=Value(0, output_field=IntegerField()),
+        discounted_price=F('price') - F('discount_amount'),
+        avg_rate=Avg('comments__rate'),
+        num_comment=Count('comments', distinct=True),
+    ).values(
+        'id', 'title', 'coach_profile', 'num_members', 'num_videos', 'product_type',
+        'num_courses', 'discounted_price', 'avg_rate', 'num_comment', 'date_created', 'date_modified', 'img'
+    )
+
+    return queryset
+
+
+def get_packages_queryset():
+    """
+        Retrieve a queryset of active packages with annotated fields.
+    """
+    package_content_type = ContentType.objects.get_for_model(Package)
+    queryset = Package.objects.filter(status=True).annotate(
+        coach_profile=Value(None, output_field=CharField()),
+        num_members=Count('memberships', filter=Q(memberships__content_type=package_content_type), distinct=True),
+        num_videos=Value(0, output_field=IntegerField()),
+        product_type=Value(2, output_field=IntegerField()),  # 2 = package
+        num_courses=Count('courses'),
+        discounted_price=F('price') - F('discount_amount'),
+        avg_rate=Avg('courses__comments__rate'),
+        num_comment=Count('courses__comments', distinct=True),
+    ).values(
+        'id', 'title', 'coach_profile', 'num_members', 'num_videos', 'product_type',
+        'num_courses', 'discounted_price', 'avg_rate', 'num_comment', 'date_created', 'date_modified', 'img'
+    )
+
+    return queryset
+
+
+def get_combined_course_package_queryset():
+    """
+        Retrieve a combined queryset of active courses & packages.
+    """
+    courses_queryset = get_courses_queryset()
+    packages_queryset = get_packages_queryset()
+
+    combined_queryset = courses_queryset.union(packages_queryset, all=True).order_by('-date_created', '-date_modified')
+    return combined_queryset
