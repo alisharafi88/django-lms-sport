28edbd45a668d925c260fedec25786004e4832b3
AliSharafi88 <alisharafi893@gmail.com>
2025-02-23 09:42:44 +0330

    Update settings for poduction.

---
+++ src/config/settings/production.py
@@ -0,0 +1,28 @@
+from .development import *
+
+
+if os.getenv('DJANGO_ENV') == 'production':
+    # === DEBUG ===
+    DEBUG = False
+
+    # === ALLOWED_HOSTS ===
+    ALLOWED_HOSTS = [
+        '*'
+    ]
+
+    # === Database ===
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST', 'localhost'),
+            'PORT': os.getenv('DB_PORT', '3306'),
+        }
+    }
+
+    # === Security ===
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
