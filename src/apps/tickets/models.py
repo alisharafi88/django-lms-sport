6cbd4542cbf5dbf796aba738412fad5c728be331
AliSharafi88 <alisharafi893@gmail.com>
2025-03-26 03:07:26 +0330

    Update tickets app tranlation.

--- src/apps/tickets/models.py
+++ src/apps/tickets/models.py
@@ -36,12 +36,12 @@ class Ticket(models.Model):
     )
 
     date_created = models.DateTimeField(
-        _('Date Created'),
+        _('Create Date'),
         auto_now_add=True,
     )
 
     date_updated = models.DateTimeField(
-        _('Date Updated'),
+        _('Update Date'),
         auto_now=True,
     )
 
@@ -49,14 +49,15 @@ class Ticket(models.Model):
         return f"{self.subject} - {self.get_status_display()}"
 
     class Meta:
-        verbose_name = _('Ticket')
-        verbose_name_plural = _('Tickets')
         ordering = ('-date_updated', '-date_created')
 
         indexes = [
             models.Index(fields=['date_updated', 'date_created'], name='date_idx'),
             models.Index(fields=['user'], name='user_idx'),
         ]
+
+        verbose_name = _('Ticket')
+        verbose_name_plural = _('Tickets')
 
 
 class TicketReply(models.Model):
@@ -77,13 +78,14 @@ class TicketReply(models.Model):
     )
 
     def __str__(self):
-        return f"Reply for Ticket ({self.ticket.get_status_display()}) on {self.ticket.subject}"
+        return _(f'Reply for Ticket ({self.ticket.get_status_display()}) on {self.ticket.subject}')
 
     class Meta:
-        verbose_name = _('Reply')
-        verbose_name_plural = _('Replies')
         ordering = ('-date_created',)
         indexes = [
             models.Index(fields=['ticket'], name='replies_ticket_idx'),
             models.Index(fields=['date_created'], name='replies_date_idx'),
         ]
+
+        verbose_name = _('Reply')
+        verbose_name_plural = _('Replies')
