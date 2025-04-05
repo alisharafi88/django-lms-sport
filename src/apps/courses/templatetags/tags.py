ba6ac381269674be8f1ad3ca75b227b2a39ec732
AliSharafi88 <alisharafi893@gmail.com>
2025-04-05 10:37:11 +0330

    Update course detail to add enrollment status.

--- src/apps/courses/templatetags/tags.py
+++ src/apps/courses/templatetags/tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.contrib.contenttypes.models import ContentType
+
+from apps.courses.models import CourseMembership
 
 register = template.Library()
 
@@ -7,3 +10,16 @@ register = template.Library()
 def is_active(obj):
     results = obj.filter(status=True)
     return set(results)
+
+
+@register.filter
+def is_enrolled(course, user):
+    if not user.is_authenticated:
+        return False
+    content_type = ContentType.objects.get_for_model(course)
+    return CourseMembership.objects.filter(
+        user=user,
+        content_type=content_type,
+        object_id=course.id
+    ).exists()
+
